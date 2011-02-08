1e6f95087cc76f09cb281bd7ec1523d3b8ba9cc9
Joachim Schleicher <joachim.schleicher@iwr.uni-heidelberg.de>
2011-02-08 11:19:12 +0100

    added va_reference: Create Reference images for tests of FPGA algorithms
    --HG--
    branch : testFixedPointResize

---
+++ va_reference/sif2tiff.cpp
@@ -0,0 +1,83 @@
+/************************************************************************/
+/*                                                                      */
+/*                  ANALYSIS OF STORM DATA                              */
+/*                                                                      */
+/*         Copyright 2010 by Joachim Schleicher and Ullrich Koethe      */
+/*                                                                      */
+/*    Please direct questions, bug reports, and contributions to        */
+/*    joachim.schleicher@iwr.uni-heidelberg.de                          */
+/************************************************************************/
+
+
+#include <iostream>
+#include <fstream>
+#include <vigra/impex.hxx>
+#include <vigra/sifImport.hxx>
+#include <vigra/utilities.hxx>
+
+
+
+using namespace vigra;
+using namespace vigra::functor;
+
+
+// MAIN
+int main(int argc, char** argv) {
+
+	if(argc != 3) {
+		std::cout << "usage: " << argv[0] << " infile.sif outfile" << std::endl << std::endl;
+		std::cout << "Converts one sif file to tiff stack" << std::endl;
+	}
+
+	std::string infile = argv[1];
+	std::string outfile = argv[2];
+	
+    try
+    {
+
+		MultiArray<3,float> in;
+		typedef MultiArray<3, float>::difference_type Shape;
+
+		int width, height, stacksize;
+		SIFImportInfo info(infile.c_str());
+		width = info.width();
+		height = info.height();
+		stacksize = info.stacksize();
+
+		// create a 3D array of appropriate size
+		in.reshape(Shape(info.width(), info.height(), info.stacksize()));
+		readSIF(info, in); //Eingabe Bild
+
+		std::cout << "Images with Shape: " << Shape(width, height, stacksize) << std::endl;
+		std::cout << "Processing a stack of " << stacksize << " images..." << std::endl;
+
+
+
+		for(unsigned int i = 0; i < stacksize; i++) {
+		//~ for(int i = 0; i < 10; i++) {
+			MultiArrayView <2, float> array = in.bindOuter(i); // select current image
+
+			BasicImageView<float> slice = makeBasicImageView(array);  // access data as BasicImage
+			
+			std::string out_nn = outfile + 
+				asString(i) + 
+				std::string(".tif");
+			exportImage(srcImageRange(slice), ImageExportInfo(out_nn.c_str()).setPixelType("UINT16"));
+			if(i%10==9) {
+				std::cout << i+1 << " ";   // 
+				flush(std::cout);
+			}
+		}
+		
+
+
+    }
+    catch (vigra::StdException & e)
+    {
+        std::cout<<"There was an error:"<<std::endl;
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+	
+	
+}
