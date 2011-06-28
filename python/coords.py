1b7393208d292023659d55eedb411964390855b3
Joachim Schleicher <joachim.schleicher@iwr.uni-heidelberg.de>
2011-06-28 18:33:30 +0200

    coords.py: Some changes from colorcomposer, plotting gaussian psf spots

--- python/coords.py
+++ python/coords.py
@@ -4,12 +4,25 @@ import csv
 import sys
 import numpy as np
 import scipy.stats
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def readfile(filename):
 	''' read file with coordinates of found spots and 
 	convert it into a numpy-array'''
 	
+	try:	# try to load cached numpy file format (faster)
+		npzfilename = str(filename)
+		if npzfilename[-4:] != '.npz':
+			npzfilename += '.npz'
+		data = np.load(npzfilename)
+		dims = data['dims']
+		coords = data['coords']
+		print "I read the cached file instead of parsing a .txt file"
+		return dims, coords
+	except IOError: # file could not be read
+		pass
+
+	# read csv coordinates list
 	csvfile = open(filename, "rb")
 	dialect = csv.Sniffer().sniff(csvfile.read(1024))
 	csvfile.seek(0)
@@ -25,8 +38,10 @@ def readfile(filename):
 	except csv.Error, e:
 		sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
 		
-	npcoords = np.array(coords, dtype='float') #numpy-array
-	return (dimline[0],dimline[1]), npcoords
+	npcoords = np.asarray(coords, dtype='float') #numpy-array
+	npdims = np.asarray(dimline[0:3])
+	np.savez(str(filename), dims=npdims, coords=npcoords)
+	return npdims, npcoords
 
 
 def writefile(filename, shape, coords):
@@ -44,34 +59,63 @@ def writefile(filename, shape, coords):
 
 def cropROI(coords, roi):
 	'''select only spots inside the roi=[xmin, xmax, ymin, ymax]'''
-	
+
 	xmin, xmax, ymin, ymax = roi
-	idx1 = coords[:,0] >= xmin
-	idx2 = coords[:,0] <  xmax
-	idx3 = coords[:,1] >= ymin
-	idx4 = coords[:,1] <  ymax
-	
-	idxs = np.all([idx1,idx2,idx3,idx4], axis=0)
-	
+	idxs = (coords[:,0] >= xmin) & (coords[:,0] <  xmax) & (coords[:,1] >= ymin) & (coords[:,1] <  ymax)
+
 	return coords[idxs,:]
 
 def coords2Image(dimension, coords, factor=8):
 	im = np.zeros((dimension[1]*factor, dimension[0]*factor))
-	for c in coords:
-		x,y, intensity = c[0], c[1], c[3]
-		im[y*factor,x*factor] += intensity
+	cc = np.array(coords[:,:2]*factor, dtype=int) # integer indices
+	for i in xrange(len(coords)):
+		intensity = coords[i,3]
+		im[cc[i,1],cc[i,0]] += intensity
 	#limit maximum value
 	mmx = scipy.stats.scoreatpercentile(im.flat, 99.6)
-	im[im>mmx] = mmx
+	if mmx > 0:
+		im[im>mmx] = mmx # crop maximum at above percentile
+	return im
+
+def intensity2Sigma(intensity):
+	return 10./float(intensity) + 0.05
+
+def gauss(intensity, sigma):
+	c0 = 1/(2.*np.pi*sigma)*1.
+	c1 = 1/(2.*np.pi*sigma)*np.exp(-1./(2.*sigma**2))
+	c2 = 1/(2.*np.pi*sigma)*np.exp(-4./(2.*sigma**2))
+	cs2 = 1/(2.*np.pi*sigma)*np.exp(-2./(2.*sigma**2))
+	cs5 = 1/(2.*np.pi*sigma)*np.exp(-5./(2.*sigma**2))
+	cs8 = 1/(2.*np.pi*sigma)*np.exp(-8./(2.*sigma**2))
+	return np.array([\
+		[cs8, cs5, c2, cs5, cs8], \
+		[cs5, cs2, c1, cs2, cs5], \
+		[c2, c1, c0, c1, c2], \
+		[cs5, cs2, c1, cs2, cs5],\
+		[cs8, cs5, c2, cs5, cs8]\
+		])*intensity
+
+def coords2ImageSmooth(dimension, coords, factor=8):
+	im = np.zeros((dimension[1]*factor, dimension[0]*factor))
+	cc = np.array(coords[:,:2]*factor, dtype=int) # integer indices
+	for i in xrange(len(coords)):
+		intensity = coords[i,3]
+		sigma = intensity2Sigma(intensity)
+		im[cc[i,1]-2:cc[i,1]+3,cc[i,0]-2:cc[i,0]+3] += gauss(intensity, sigma*factor)
+	#limit maximum value
+	mmx = scipy.stats.scoreatpercentile(im.flat, 99.6)
+	if mmx > 0:
+		im[im>mmx] = mmx # crop maximum at above percentile
 	return im
 
 if __name__ == "__main__":
 	'''read image coordinates, construct images
 	save output as .png or .tiff ...'''
 	if len(sys.argv) < 3:
-		print ( "Usage: " + sys.argv[0] + "coordsfile.txt image.png")
+		print ( "Usage: " + sys.argv[0] + " coordsfile.txt image.png")
 		sys.exit(1)
+	import matplotlib.pyplot as plt
 	dims, coords = readfile(sys.argv[1])
-	im = coords2Image(dims, coords, factor=10)
+	im = coords2Image(dims, coords, factor=8)
 	plt.gray()
 	plt.imsave(sys.argv[2], im)
